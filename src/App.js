d27ef89a2c36ddbb39356eb109db3a29564e8ca8
tuananh0303 <anhhello564@gmail.com>
2023-05-14 15:06:56 +0700

    project Spotify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import DetailSong from "./components/DetailSongs";
+import ListSongs from "./components/ListSongs";
+import { Songs } from "./Context";
+import DataSongs from "./datasong/songs.json";
+import Playing from "./components/PlayingAudio";
+import { useState } from "react";
 function App() {
+  const [song, setSong] = useState(DataSongs[0]);
+
+  const handleSetSong = (idSong) => {
+    const song = DataSongs.find((song) => song.id === idSong);
+    if (!song) setSong(DataSongs[0]);
+    else setSong(song);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Songs.Provider value={{ DataSongs, song, handleSetSong }}>
+        <Header />
+        <div className="grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden">
+          {/* span 1 */}
+          <DetailSong />
+          {/* span 2 */}
+          <ListSongs />
+        </div>
+        <Playing />
+      </Songs.Provider>
     </div>
   );
 }
